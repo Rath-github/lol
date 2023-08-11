dd768988dae6203af3ed1f765645e5b7e2478f31
KenjiKuroda <103075517+KenjiKuroda@users.noreply.github.com>
2023-08-11 16:00:12 -0300

    Create cliente.component.spec.ts

---
+++ src/app/cliente/cliente.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClienteComponent } from './cliente.component';
+
+describe('ClienteComponent', () => {
+  let component: ClienteComponent;
+  let fixture: ComponentFixture<ClienteComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ClienteComponent]
+    });
+    fixture = TestBed.createComponent(ClienteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
