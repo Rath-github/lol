72c5410281021bd3b5f38cd331d429fbaadd413e
Igor <igornathan20@hotmail.com>
2023-08-14 14:43:46 -0300

    Remoção do modulo de login, para deixar direto no modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {LoginModule} from './login';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LoginModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/index.ts
+++ src/app/login/index.ts
@@ -1,1 +1,1 @@
-export * from './login.module'
+export * from './login.component'
