1e6c166c6ac7a39a03af676b0325ae6ac0aae3e5
IamThiago-IT <thiagodsantos316@gmail.com>
2023-08-21 14:40:37 -0300

    pagamento

---
+++ src/app/confirmar-pagamento/confirmar-pagamento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmar-pagamento',
+  templateUrl: './confirmar-pagamento.component.html',
+  styleUrls: ['./confirmar-pagamento.component.css']
+})
+export class ConfirmarPagamentoComponent {
+
+}
