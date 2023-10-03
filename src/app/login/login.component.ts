247e10ae237fc46d710ba8928517ff9b96272d76
igornathan20 <igornathan20@hotmail.com>
2023-10-02 21:48:25 -0300

    ajuste no componente de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,6 +11,7 @@ export class LoginComponent implements OnInit {
   usuario : string = '';
   senha : string = '';
   loginIncorreto : boolean = false;
+  acessoLogin : string = '';
   
   
 
@@ -18,19 +19,81 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   
-
 }
+usuarios : any[] = [
+  {
+    id: 1,
+    cpf : "123.456.789-00",
+    nome: "João",
+    email : "joao@gmail.com",
+    endereco : "Rua A, 123",
+    telefone : "(11) 1234-5678",
+    senha : "1234",
+    tipo : "cliente"
+  },
+  {
+    id : 2,
+    cpf : "987.654.321-00",
+    nome : "José",
+    email : "jose@gmail.com",
+    endereco : "Rua B, 456",
+    telefone : "(11) 9876-5432",
+    senha : "1234",
+    tipo : "cliente"
+  },
+  {
+    id : 3,
+    cpf : "101.213.141-51",
+    nome : "Joana",
+    email : "joana@gmail.com",
+    endereco : "Rua C, 789",
+    telefone : "(11) 1012-1130",
+    senha : "1234",
+    tipo : "cliente"
+  },
+  {
+    id : 1,
+    cpf : "111.222.333-44",
+    nome : "Maria",
+    email : "maria@gmail.com",
+    senha : "1234",
+    cargo : "Atendente",
+    tipo : "funcionario"
+  },
+  {
+    id : 2,
+    cpf : "222.333.444-55",
+    nome : "Mário",
+    email : "mario@gmail.com",
+    senha : "1234",
+    cargo : "Gerente",
+    tipo : "funcionario"
+  }
+];
+
+
 login(){
 
+  const usuarioEncontrado = this.usuarios.find(
+    (user) => user.email === this.usuario && user.senha === this.senha
+  );
  
-    if(this.usuario === "user@ufpr.br" && this.senha === "1234"){
+  if (usuarioEncontrado) {
+      
       this.estados.alterarValor(true);
-      this.router.navigate(['/cliente']);
 
-    
-    }
-    else{
-      this.loginIncorreto = true;
-    }
+      if (usuarioEncontrado.tipo === 'cliente') {
+        this.router.navigate(['/cliente']);
+        this.acessoLogin = 'cliente';
+      } 
+      else if (usuarioEncontrado.tipo === 'funcionario') {
+        this.router.navigate(['/funcionario']);
+        this.acessoLogin = 'funcionario';
+      }
+ }
+   
+  else{
+    this.loginIncorreto = true;
+  }
 }
 }
