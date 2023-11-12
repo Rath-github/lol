001d5671de44a1bafc100f0e1dd26ff8a41c73f1
IamThiago-IT <thiagodossantos315@gmail.com>
2023-11-12 01:18:51 -0300

    Visualização de pedidos

--- src/app/funcionario/funcionario.component.ts
+++ src/app/funcionario/funcionario.component.ts
@@ -7,37 +7,99 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./funcionario.component.css'],
 })
 export class FuncionarioComponent implements OnInit {
-  filtroSelecionado: string = 'aberto';
+  filtroSelecionado: string = 'todos';
   dataInicio: string = '';
   dataFim: string = '';
+  pedidos: any[] = [];
+  perfil: string = 'funcionario'; // Defina o perfil atual
   pedidosAbertosNaoRecolhidos: any[] = [];
+  pedidosFiltrados: any[] = []; // Adicione esta linha
+  pedidosOriginais: any[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.filtrarPedidosAbertosNaoRecolhidos();
+    this.carregarPedidos();
+    
   }
 
-  filtrarPedidosAbertosNaoRecolhidos(): void {
+  carregarPedidos(): void {
     this.http.get<any[]>('http://localhost:3333/pedidos').subscribe((pedidos) => {
-      this.pedidosAbertosNaoRecolhidos = pedidos.filter(
-        (pedido) => pedido.pedidoEstado === 'EM ABERTO' && !pedido.pedidoRecolhido
-      );
+      this.pedidosOriginais = pedidos;
+      this.filtrarPedidos();
     });
   }
 
-  registrarRecolhimento(pedido: any): void {
-    pedido.pedidoEstado = 'RECOLHIDO';
-    pedido.pedidoRecolhido = true;
+  filtrarPedidos(): void {
+    const hoje = new Date();
+    hoje.setHours(0, 0, 0, 0); // Zerar horas, minutos, segundos e milissegundos
 
-    this.http.put(`http://localhost:3333/pedidos/${pedido.id}`, pedido).subscribe(
-      () => {
-        // Pedido atualizado com sucesso
-      },
-      (error) => {
-        // Tratar erros, se necessário
-      }
-    );
+    if (this.filtroSelecionado === 'todos') {
+      this.pedidos = this.pedidosOriginais;
+    } else if (this.filtroSelecionado === 'hoje') {
+      this.pedidos = this.pedidosOriginais.filter(pedido =>
+        new Date(pedido.pedidoAno, pedido.pedidoMes - 1, pedido.pedidoDia) >= hoje
+      );
+    } else if (this.filtroSelecionado === 'periodo') {
+      const dataInicio = new Date(this.dataInicio);
+      const dataFim = new Date(this.dataFim);
+      dataFim.setHours(23, 59, 59, 999); // Configurar para o final do dia
+
+      this.pedidos = this.pedidosOriginais.filter(pedido =>
+        new Date(pedido.pedidoAno, pedido.pedidoMes - 1, pedido.pedidoDia) >= dataInicio &&
+        new Date(pedido.pedidoAno, pedido.pedidoMes - 1, pedido.pedidoDia) <= dataFim
+      );
+    }
+  }
+
+
+  registrarRecolhimento(pedido: any): void {
+    // Verificar se o pedido está em um estado adequado para recolhimento
+    if (pedido.pedidoEstado === 'EM ABERTO') {
+      pedido.pedidoEstado = 'RECOLHIDO';
+      pedido.pedidoRecolhido = true;
+
+      this.http.put(`http://localhost:3333/pedidos/${pedido.id}`, pedido).subscribe(
+        () => {
+          // Pedido atualizado com sucesso
+        },
+        (error) => {
+          // Tratar erros, se necessário
+        }
+      );
+    }
+  }
+
+  confirmarLavagem(pedido: any): void {
+    // Verificar se o pedido está em um estado adequado para confirmação de lavagem
+    if (pedido.pedidoEstado === 'RECOLHIDO') {
+      pedido.pedidoEstado = 'AGUARDANDO PAGAMENTO';
+
+      this.http.put(`http://localhost:3333/pedidos/${pedido.id}`, pedido).subscribe(
+        () => {
+          // Pedido atualizado com sucesso
+        },
+        (error) => {
+          // Tratar erros, se necessário
+        }
+      );
+    }
+  }
+
+  finalizarPedido(pedido: any): void {
+    // Verificar se o pedido está em um estado adequado para finalização
+    if (pedido.pedidoEstado === 'PAGO') {
+      pedido.pedidoEstado = 'FINALIZADO';
+
+      this.http.put(`http://localhost:3333/pedidos/${pedido.id}`, pedido).subscribe(
+        () => {
+          // Pedido atualizado com sucesso
+        },
+        (error) => {
+          // Tratar erros, se necessário
+        }
+      );
+    }
   }
 
   formatarData(dia: number, mes: number, ano: number, hora: number): string {
