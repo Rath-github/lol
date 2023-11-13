e4d43791d38a7c4d81e7cf8600fb3442bc41eaf8
IamThiago-IT <thiagodossantos315@gmail.com>
2023-11-13 18:50:23 -0300

    CRUD roupas completo

--- src/app/roupas/roupas.component.ts
+++ src/app/roupas/roupas.component.ts
@@ -1,71 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Roupa } from './roupaModelo/roupa.model';
-
 
 @Component({
   selector: 'app-roupas',
   templateUrl: './roupas.component.html',
   styleUrls: ['./roupas.component.css']
 })
-export class RoupasComponent {
-  pecas: Roupa[]=[
-    new Roupa(1,'camisa',50,2),
-    new Roupa(2,'calca',100,3),
-    new Roupa(3,'vestido',150,4)];
+export class RoupasComponent implements OnInit {
+  pecas: Roupa[] = [];
+  campoEditar: boolean = false;
+  campoNovo: boolean = false;
+  nomeSelecionado: string = '';
+  roupaSelecionada: Roupa = new Roupa(0, '', 0, 0);
+  novaRoupa: Roupa = new Roupa(0, '', 0, 0);
 
-    campoEditar: boolean = false;
-    campoNovo: boolean = false;
-    nomeSelecionado: string='';
-    
-    roupaSelecionada: Roupa = new Roupa(0, '', 0, 0);
-    novaRoupa: Roupa = new Roupa(0, '', 0, 0);
-    
-    
-    
-  remover(id:number){
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.carregarRoupas();
+  }
+
+  carregarRoupas(): void {
+    this.http.get<Roupa[]>('http://localhost:3333/pecas').subscribe(pecas => {
+      this.pecas = pecas;
+    });
+  }
+
+  remover(id: number): void {
     if (confirm(`Deseja realmente remover o item: ${id}?`)) {
-      this.pecas = this.pecas.filter(Roupa=> Roupa.id !=id);
-      }
-  
+      this.http.delete(`http://localhost:3333/pecas/${id}`).subscribe(() => {
+        this.carregarRoupas();
+      });
+    }
   }
 
-  selecionarEditar(id: number){
-  
-    this.roupaSelecionada = this.pecas.find((Roupa) => Roupa.id === id) || new Roupa(0, '', 0, 0);
-
-    this.campoEditar = true;
-    this.campoNovo=false;
-    console.log(this.roupaSelecionada.id);
-
-    this.novaRoupa = { ...this.roupaSelecionada }; // o operador ... (spread) cria uma copia do conteudo da variavel sem que elas compartilhem do mesmo objeto
-    
+  selecionarEditar(id: number): void {
+    this.http.get<Roupa>(`http://localhost:3333/pecas/${id}`).subscribe(roupa => {
+      this.roupaSelecionada = roupa || new Roupa(0, '', 0, 0);
+      this.campoEditar = true;
+      this.campoNovo = false;
+      this.novaRoupa = { ...this.roupaSelecionada };
+    });
   }
 
-  editar(){
-    const index = this.pecas.findIndex((roupa) => roupa.id === this.roupaSelecionada.id);
-    this.novaRoupa.id = this.roupaSelecionada.id;
-    this.roupaSelecionada = this.novaRoupa ; 
-
-    this.pecas[index] = this.roupaSelecionada; 
-    
-    this.campoEditar = false;
-
-    console.log(this.pecas[index].nome);
-    console.log(this.roupaSelecionada.nome);
-  
+  editar(): void {
+    this.http.put(`http://localhost:3333/pecas/${this.roupaSelecionada.id}`, this.novaRoupa).subscribe(() => {
+      this.carregarRoupas();
+      this.campoEditar = false;
+    });
   }
 
-  novo(){
-    let maiorId = this.pecas.reduce((maxId, roupa) => {
-      return Math.max(maxId, roupa.id);
-    }, 0);
-
-   
-    const roupaNova = new Roupa (maiorId + 1, this.novaRoupa.nome, this.novaRoupa.preco, this.novaRoupa.tempo);
-
-    this.pecas.push(roupaNova);
-
-    this.campoNovo=false;
+  novo(): void {
+    this.http.post('http://localhost:3333/pecas', this.novaRoupa).subscribe(() => {
+      this.carregarRoupas();
+      this.campoNovo = false;
+    });
   }
-
 }
