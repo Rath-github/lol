03958d7d48bd0d808fca26e28083f230c4527a45
IamThiago-IT <thiagodossantos315@gmail.com>
2023-11-17 23:21:47 -0300

    Update Relatorio receita

--- src/app/relatorio-receitas/relatorio-receitas.component.ts
+++ src/app/relatorio-receitas/relatorio-receitas.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { jsPDF } from 'jspdf'; 
+import { HttpClient } from '@angular/common/http';
+import { jsPDF } from 'jspdf';
 
 interface Receita {
-  data: string;
-  valor: number;
+  pedidoNum: number;
+  pedidoPrazo: string;
+  pedidoOrcamento: string;
+  pedidoDia: number;
+  pedidoMes: number;
+  pedidoAno: number;
+  pedidoRecolhido: boolean;
+  pedidoRoupas: { tipo: string; quantidade: number; tempo: number; preco: number }[];
 }
-
 
 @Component({
   selector: 'app-relatorio-receitas',
@@ -13,30 +19,101 @@ interface Receita {
   styleUrls: ['./relatorio-receitas.component.css']
 })
 export class RelatorioReceitasComponent implements OnInit {
-  receitas: Receita[] = [
-    { data: '2023-08-01', valor: 150 },
-    { data: '2023-08-02', valor: 200 },
-    { data: '2023-08-03', valor: 180 },
-  
-  ];
+  receitas: Receita[] = [];
+  logo: string = '/assets/img/logo.png';
+  dataInicio: string = ''; // Data inicial do filtro
+  dataFim: string = ''; // Data final do filtro
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.carregarReceitas();
+  }
+
+  carregarReceitas(): void {
+    this.http.get<any[]>('http://localhost:3333/pedidos').subscribe((data) => {
+      this.receitas = [...data];
+    });
+  }
+
+  filtrarPorData(): Receita[] {
+    let receitasFiltradas: Receita[] = [...this.receitas];
+
+    if (this.dataInicio && this.dataFim) {
+      const inicio = new Date(this.dataInicio);
+      const fim = new Date(this.dataFim);
+
+      receitasFiltradas = receitasFiltradas.filter((receita) => {
+        const receitaData = new Date(
+          receita.pedidoAno,
+          receita.pedidoMes - 1,
+          receita.pedidoDia
+        );
+        return receitaData >= inicio && receitaData <= fim;
+      });
+    }
+
+    return receitasFiltradas;
+  }
 
   gerarRelatorio(): void {
+    const receitasFiltradas = this.filtrarPorData();
+
     const doc = new jsPDF();
-    doc.text('Relatório de Receitas', 10, 10);
 
-    let y = 30; 
+    doc.addImage(this.logo, 'PNG', 10, 0, 50, 50);
 
-    for (const receita of this.receitas) {
-      doc.text(`Data: ${receita.data}`, 10, y);
-      doc.text(`Valor: R$ ${receita.valor.toFixed(2)}`, 60, y);
+    doc.setFont('Georgia');
+    doc.setFontSize(26);
+    doc.text('Relatório de Receitas', 80, 25);
 
-      y += 10; 
+    const dataAtual = new Date();
+    const dataFormatada = dataAtual.toLocaleDateString('pt-BR', { year: 'numeric', month: 'long', day: 'numeric' });
+
+    doc.setFontSize(12);
+    doc.text(`Data: ${dataFormatada}`, 93, 31);
+
+    // Agrupar por dia
+    const receitasAgrupadas = this.agruparPorDia(receitasFiltradas);
+
+    let y = 70; // Posição vertical para começar a adicionar os dados
+
+    doc.setFontSize(13);
+    for (const [data, receitasPorDia] of receitasAgrupadas) {
+      if (y > 250) {
+        doc.addPage(); // Adiciona uma nova página se a altura exceder 250
+        y = 20; // Reinicia a posição vertical
+      }
+
+      doc.setFont('Arial', 'bold');
+      doc.setFontSize(16);
+      doc.text(`Receita do dia: ${data}`, 14, y);
+
+      y += 10; // Incremento da posição vertical
+
+      // Detalhes dos pedidos para o dia
+      receitasPorDia.forEach((receita: Receita) => {
+        doc.setFont('Arial', 'normal');
+        doc.setFontSize(12);
+        doc.text(`Pedido Número: ${receita.pedidoNum}`, 14, y);
+        doc.text(`Prazo: ${receita.pedidoPrazo}`, 14, y + 7);
+        doc.text(`Orçamento: ${receita.pedidoOrcamento}`, 14, y + 14);
+        // Adicionar mais detalhes conforme necessário
+
+        y += 25; // Incremento da posição vertical para o próximo pedido
+      });
+
+      y += 10; // Espaço entre dias
     }
 
     doc.save('relatorio_receitas.pdf');
+  }
+
+  agruparPorDia(receitas: Receita[]): Map<string, Receita[]> {
+    return receitas.reduce((acc: Map<string, Receita[]>, receita: Receita) => {
+      const data = `${receita.pedidoDia}/${receita.pedidoMes}/${receita.pedidoAno}`;
+      acc.set(data, (acc.get(data) || []).concat(receita));
+      return acc;
+    }, new Map<string, Receita[]>());
   }
 }
